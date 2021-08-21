d2d7b85fe11903ae0a41af448c880e08a36a313c
Omer Ahmed <dev.omer.ahmed@gmail.com>
2021-08-21 15:16:43 +0300

    init project

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name'    : "pos network arabic receipt",
+    'author'  : 'Omer Ahmed',
+    'category': 'POS',
+    'summary' : """fix issues with print arabic receipt by network """,
+    'license' : 'OPL-1',
+    'website' : 'http://github.com/omerahmed1994',
+    'description': """fix issues with print arabic receipt by network """,
+    'version' : '14.0',
+    'depends' : ['point_of_sale'],
+    'data'    : [ ],
+    'installable'  : True,
+}
